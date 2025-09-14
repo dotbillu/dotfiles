c314d09669ca1d434bbfcada2fd1b5a1c1680bc3
dotbillu <billudestroyer@outlook.com>
2025-09-15 00:15:24 +0530

    disabled virtual suggesions

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -2,6 +2,9 @@ require "nvchad.options"
 
 -- add yours here!
 
-vim.diagnostic.config({ update_in_insert = true })
+vim.diagnostic.config({ 
+  virtual_text=false,
+  update_in_insert = true 
+})
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
