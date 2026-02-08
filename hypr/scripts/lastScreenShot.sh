31900bf1ddb690cdd801560facd687fe6f0a3976
dotbillu <billudestroyer@outlook.com>
2026-02-08 15:24:28 +0530

    .

--- hypr/scripts/lastScreenShot.sh
+++ hypr/scripts/lastScreenShot.sh
@@ -1,6 +1,11 @@
 DIR="$HOME/Pictures/Screenshots"
-latest=$(find "$DIR" -type f \( -iname "*.png" -o -iname "*.jpg" -o -iname "*.jpeg" \) \
-  -printf "%T@ %p\n" 2>/dev/null | sort -nr | head -n 1 | cut -d' ' -f2-)
 
-  eog "$latest"
+if pgrep -x eog >/dev/null; then
+  pkill -x eog
+else
+  latest=$(find "$DIR" -type f \( -iname "*.png" -o -iname "*.jpg" -o -iname "*.jpeg" \) \
+    -printf "%T@ %p\n" 2>/dev/null | sort -nr | head -n 1 | cut -d' ' -f2-)
+
+  [ -n "$latest" ] && eog "$latest"
+fi
 
